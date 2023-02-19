3dd65a849f77eb8f6344d8c1ae68064cb5929bf8
TamKatja <=>
2023-02-20 07:21:39 +0800

    Added movie swiper

--- js/script.js
+++ js/script.js
@@ -223,6 +223,50 @@ function displayBackgroundImage(type, backgroundPath) {
     }
 }
 
+// Display slider movies
+async function displaySlider() {
+    const { results } = await fetchAPIData('movie/now_playing');
+    results.forEach((movie) => {
+        const div = document.createElement('div');
+        div.classList.add('swiper-slide');
+        div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+                <i class="fas fa-star text-secondary"></i>
+                ${movie.vote_average} / 10
+            </h4>`;
+        document.querySelector('.swiper-wrapper').appendChild(div);
+
+        initSwiper();
+    });
+}
+
+function initSwiper() {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        freeMode: true,
+        loop: true,
+        autoplay: {
+            delay: 4000,
+            dosableOnInteraction: false,
+        },
+        breakpoints: {
+            500: {
+                slidesPerView: 2,
+            },
+            700: {
+                slidesPerView: 3,
+            },
+            1200: {
+                slidesPerView: 4,
+            },
+        },
+    });
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
     const API_KEY = '58d8325e942989954ae55037aa14f785'; // Hide in production
@@ -257,6 +301,7 @@ function init() {
     switch (global.currentPage) {
         case '/':
         case '/index.html':
+            displaySlider();
             displayPopularMovies();
             break;
         case '/shows.html':
